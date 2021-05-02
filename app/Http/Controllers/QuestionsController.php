8cc7ec657f1bc048eed281e701fd55af09e5cd29
javedwasim <javedafaq@gmail.com>
2021-05-02 10:05:44 +0500

    implement policy on edit and delete

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except' => ['index','show'] ]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -65,6 +69,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize('update',$question);
         return view('questions.edit',compact('question'));
     }
 
@@ -77,6 +82,7 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update',$question);
         $question->update($request->only('title','body'));
 
         return redirect()->route('questions.index')->with('success','Your Question has been updated');
@@ -90,6 +96,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete',$question);
         $question->delete();
         return redirect()->route('questions.index')->with('success','Question has been deleted successfully!');
     }
